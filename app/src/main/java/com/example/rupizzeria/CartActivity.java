12800e4c02bbaf23290d978768711cf21a1d4879
vytngn <vytnguyenp@gmail.com>
2024-11-29 23:17:02 -0500

    fix home page, back button in cart

--- app/src/main/java/com/example/rupizzeria/CartActivity.java
+++ app/src/main/java/com/example/rupizzeria/CartActivity.java
@@ -1,6 +1,8 @@
 package com.example.rupizzeria;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class CartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        // Initialize back button
+        ImageButton backButton = findViewById(R.id.btn_back);
+        backButton.setOnClickListener(v -> navigateBackToHome());
+    }
+
+    /**
+     * Navigates back to the MainActivity.
+     */
+    private void navigateBackToHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Optional: Clears other activities
+        startActivity(intent);
+        finish(); // End current activity
     }
 }
